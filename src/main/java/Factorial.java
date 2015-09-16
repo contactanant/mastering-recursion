11f53b23a6498e9a8c2d1fb927f8dfa376b8e309
Anant Agarwal <shwetamona@gmail.com>
2015-09-16 21:14:42 +0100

    Anant | Solved factorial problem

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,19 @@
+/**
+ courtesy http://codingbat.com/prob/p154669
+
+ Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+
+ factorial(1) → 1
+ factorial(2) → 2
+ factorial(3) → 6
+ */
+public class Factorial {
+ public int calculateFactorial(int n) {
+  if (n < 1) return 0;
+  return n == 1 ? 1 : n * calculateFactorial(n - 1);
+ }
+
+ public static void main(String[] args) {
+  System.out.println(new Factorial().calculateFactorial(3));
+ }
+}
